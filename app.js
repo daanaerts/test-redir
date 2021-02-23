470ab68d1db068c526c6f0149a33774147b3fae4
Daan Aerts <daanaerts@protonmail.com>
2021-02-23 20:44:28 +0100

    x

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// eslint-disable-next-line import/no-unresolved
+const express = require("express");
+const pick = require("lodash.pick");
+const app = express();
+
+// Routes
+app.get("/*", (req, res) => {
+  const x = pick(req, ["baseUrl", "hostname", "originalUrl"]);
+  res.send(`Request received: ${JSON.stringify(x)}`);
+});
+
+// Error handler
+app.use((err, req, res) => {
+  console.error(err);
+  res.status(500).send("Internal Serverless Error");
+});
+
+module.exports = app;
