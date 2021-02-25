637e1e641569c785161e63cbf193108fb5ccb3dd
Daan Aerts <daanaerts@protonmail.com>
2021-02-25 13:50:21 +0100

    update x

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 COPY . .
 RUN npm install
-RUN npm build
-
+RUN npm run build
+ENV PORT=8080
 EXPOSE 8080
-CMD [ "node", "server.js" ]
+CMD [ "npm", "start" ]
--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,11 @@ const app = express();
 app.get("/*", (req, res) => {
   console.log("received req");
   const x = pick(req, ["baseUrl", "hostname", "originalUrl"]);
-  console.log(x);
+
   res.send(`Request received: ${JSON.stringify(x)}`);
 });
 
-app.set("port", process.env.port || 5000);
+app.set("port", process.env.PORT || 5000);
 console.log("initializing");
 app.listen(app.get("port"), () => {
   console.log(`Express listening on ${app.get("port")}`);
